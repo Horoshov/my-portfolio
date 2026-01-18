b1af51938fe8b4b361741edaffec17c20a639e42
Alex Horoshov <horoshov@bk.ru>
2026-01-19 01:15:45 +0300

    Final fix for GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import legacy from '@vitejs/plugin-legacy'
 
-
 export default defineConfig({
-  base: '/', 
+  base: '/',
   plugins: [
     react(),
     legacy({
