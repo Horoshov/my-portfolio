4e39bf32a9cdfb84e23cd0e1e28a1798bec56e14
Alex Horoshov <horoshov@bk.ru>
2026-01-21 13:17:57 +0300

    styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => (
     <div className="nav-col">
       <span className="nav-label">Navigation</span>
       <div className="nav-menu">
-        {['Home', 'Projects', 'Carier', 'Articles', 'About', 'Contact'].map(item => (
+        {['Home', 'Projects', 'Articles', 'About', 'Contact'].map(item => (
           <Link key={item} to={item === 'Home' ? '/' : `/${item.toLowerCase()}`} className="nav-menu-item">
             {item}
           </Link>
