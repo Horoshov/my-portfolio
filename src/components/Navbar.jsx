c70efc1fb685a5a0e37bce29c4d4cc47c3804c1a
Alex Horoshov <horoshov@bk.ru>
2026-01-24 02:50:13 +0300

    about

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -75,9 +75,11 @@ const Navbar = () => {
             onClick={toggleMenu}
             aria-label="Toggle menu"
           >
-            <span></span>
-            <span></span>
-            <span></span>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M20.75 7C20.75 7.41421 20.4142 7.75 20 7.75H4C3.58579 7.75 3.25 7.41421 3.25 7C3.25 6.58579 3.58579 6.25 4 6.25H20C20.4142 6.25 20.75 6.58579 20.75 7Z" fill="currentColor"/>
+            <path d="M20.75 12C20.75 12.4142 20.4142 12.75 20 12.75H4C3.58579 12.75 3.25 12.4142 3.25 12C3.25 11.5858 3.58579 11.25 4 11.25H20C20.4142 11.25 20.75 11.5858 20.75 12Z" fill="currentColor"/>
+            <path d="M20.75 17C20.75 17.4142 20.4142 17.75 20 17.75H4C3.58579 17.75 3.25 17.4142 3.25 17C3.25 16.5858 3.58579 16.25 4 16.25H20C20.4142 16.25 20.75 16.5858 20.75 17Z" fill="currentColor"/>
+            </svg>
           </button>
         </div>
       </nav>
@@ -86,7 +88,7 @@ const Navbar = () => {
       <div className={`mobile-menu-overlay ${isMenuOpen ? 'active' : ''}`} onClick={closeMenu}>
         <div className="mobile-menu-content" onClick={(e) => e.stopPropagation()}>
           
-          {/* Header с логотипом и крестиком */}
+          {/* Header Ñ Ð»Ð¾Ð³Ð¾Ñ‚Ð¸Ð¿Ð¾Ð¼ Ð¸ ÐºÑ€ÐµÑÑ‚Ð¸ÐºÐ¾Ð¼ */}
           <div className="mobile-menu-header">
             <Link to="/" className="mobile-menu-logo-link" onClick={closeMenu}>
               <svg width="99" height="13" viewBox="0 0 99 13" fill="none" xmlns="http://www.w3.org/2000/svg">
