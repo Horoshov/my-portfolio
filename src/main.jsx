c955938633711bde8fc559b9234afe46e7b5afd7
Alex Horoshov <horoshov@bk.ru>
2026-01-31 15:39:13 +0300

    Optim

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css';
-// 1. Импортируем компонент
-import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    {/* 2. Подключаем аналитику рядом с основным приложением */}
-    <Analytics />
   </React.StrictMode>,
 )
