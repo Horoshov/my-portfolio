2223c8a80b60447860e6941626c13de64a463ba1
Alex Horoshov <horoshov@bk.ru>
2026-02-04 00:32:49 +0300

    Dot_2

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -19,9 +19,9 @@ const ProjectCard = ({ project }) => {
           {/* Центрированная анимированная кнопка и слой затемнения */}
           <div className="pc-hover-overlay">
             <div className="pc-center-button">
-              <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M7 17L17 7M17 7H7M17 7V17" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
-              </svg>
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M5 12H19M19 12L13 6M19 12L13 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>
             </div>
           </div>
         </div>
